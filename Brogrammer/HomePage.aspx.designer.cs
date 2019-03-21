bbadc6e42057b32b401910c60b15d684949bda72
aldochu <aldochu@gmail.com>
2019-03-21 18:12:48 +0800

    Added link to ListOfPost page

--- Brogrammer/HomePage.aspx.designer.cs
+++ Brogrammer/HomePage.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace Brogrammer {
     public partial class HomePage {
         
         /// <summary>
+        /// LinkButton1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
+        
+        /// <summary>
         /// ScriptManager1 control.
         /// </summary>
         /// <remarks>
